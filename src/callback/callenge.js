6ddf3601cf62a7068c8a0b4b59123b24b24a37e4
Hielo Elemental <hieloelemental@gmail.com>
2024-05-23 17:55:09 -0500

    feat: :pencil2: lesson 9
    use the FetchData function to get API data, also fixing requests and fetchData function

--- src/callback/callenge.js
+++ src/callback/callenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 
 const fetchData = (urlApi, callback) => {
@@ -7,11 +7,28 @@ const fetchData = (urlApi, callback) => {
   xhttp.open('GET', urlApi, true);
   xhttp.onreadystatechange = (event) => {
     if (xhttp.readyState === 4) {
-      xhttp.status === 200 && callback(null, JSON.parse(xhttp.responseText))
-    } else {
-      const error = new Error('Error' + urlApi);
-      return callback(error, null);
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error('Error' + urlApi);
+        return callback(error, null);
+      }
     }
   }
   xhttp.send();
 }
+
+fetchData(`${API}/products`, (error1, data1) => {
+  if (error1) return console.error(error1);
+  fetchData(`${API}/products/${data1[0]?.id}`, (error2, data2) => {
+    if (error2) return console.error(error2);
+    fetchData(`${API}/categories/${data2?.category?.id}`, (error3, data3) => {
+      if (error3) return console.error(error3);
+
+      console.log(data1[0]);
+      console.log(data2.title);
+      console.log(data3.name);
+    });
+  });
+});
+
